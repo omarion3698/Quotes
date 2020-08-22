ea82830fd4da3a3d2e605cbe38d7bb356444a849
omarion3698 <omaribinbakarivic@gmail.com>
2020-08-22 22:48:43 +0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesFormComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
